d6536198e744ea7df3cd2863f4c41524896b6d02
Michał Czuba <michal.czuba.1995@gmail.com>
2024-10-18 15:00:28 +0000

    removed submodule nsl_data

---
+++ src/loaders/constants.py
@@ -0,0 +1,26 @@
+"""A script with strings used across module in form of variables."""
+
+from pathlib import Path
+
+MLN_RAW_DATA_PATH = Path(__file__).parent.parent / "_data_set/raw/multi_layer_networks"
+
+# network names
+ARXIV_NETSCIENCE_COAUTHORSHIP = "arxiv_netscience_coauthorship"
+ARXIV_NETSCIENCE_COAUTHORSHIP_MATH = "arxiv_netscience_coauthorship_math.oc"
+AUCS = "aucs"
+CANNES = "cannes"
+CKM_PHYSICIANS = "ckm_physicians"
+EU_TRANSPORTATION = "eu_transportation"
+EU_TRANSPORT_KLM = "eu_transport_klm"
+ER1 = "er1"
+ER2 = "er2"
+ER3 = "er3"
+ER5 = "er5"
+FMRI74 = "fmri74"
+LAZEGA = "lazega"
+SF1 = "sf1"
+SF2 = "sf2"
+SF3 = "sf3"
+SF5 = "sf5"
+TIMIK1Q2009 = "timik1q2009"
+TOY_NETWORK = "toy_network"
