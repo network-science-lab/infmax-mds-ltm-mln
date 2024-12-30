bcc3c382845bbe88f2704750198de3809838a8c2
Michał Czuba <michal.czuba.1995@gmail.com>
2024-12-30 02:15:43 +0000

    updated README

--- visualise_mds.py
+++ visualise_mds.py
@@ -6,15 +6,16 @@ from src.aux.network_generator import generate
 from src.aux.visualise_mds import MDSPlotter
 from src.models.mds import greedy_search, local_improvement
 from src.loaders.net_loader import load_network
+from src.visualisation import Plotter
 
 
 if __name__ == "__main__":
 
-    mds_func = local_improvement.get_mds_locimpr
+    # mds_func = local_improvement.get_mds_locimpr
     mds_func = greedy_search.get_mds_greedy
 
     # prepare outout directory
-    out_dir = Path("./doodles")
+    out_dir = Path("./data/processed_results/visualisations_mds")
     out_dir.mkdir(exist_ok=True, parents=True)
 
     # visualise artificial networks
@@ -33,7 +34,7 @@ if __name__ == "__main__":
         mds_plotter.plot_structure()
 
     # visualise real networks
-    for net_name in ["aucs", "l2_course_net_1"]: # Plotter._networks:
+    for net_name in Plotter._networks:
         net = load_network(net_name, as_tensor=False)
         mds = mds_func(net)
         mds_plotter = MDSPlotter(net, mds, net_name, out_dir)
