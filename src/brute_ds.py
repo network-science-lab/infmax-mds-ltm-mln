8af3a48d9c6beb632c042bc648aa9f630b63ca00
Michał Czuba <michal.czuba.1995@gmail.com>
2025-05-30 15:05:07 +0000

    added evaluated size range

--- src/brute_ds.py
+++ src/brute_ds.py
@@ -40,25 +40,31 @@ class OutFile:
             file.write(f"{';'.join(ds)},{len(ds)}\n")
 
 
-def get_possible_ds_nb(nb_actors: int, max_eval_size: int) -> int:
+def get_possible_ds_nb(nb_actors: int, min_eval_size: int,  max_eval_size: int) -> int:
     """Get number of solutions to evaluate."""
-    possible_ds_nb = int(np.sum([sp.binom(nb_actors, n) for n in range(max_eval_size + 1)]).item())
-    print(f"Nb of up-to-{max_eval_size} elem. sets for {nb_actors} size net is {possible_ds_nb}")
+    possible_ds_nb = np.sum(
+            [sp.binom(nb_actors, n) for n in range(min_eval_size, max_eval_size + 1)]
+        ).astype(int).item()
+    print(
+        f"Number of {min_eval_size}-to-{max_eval_size} elemement sets "
+        f"for {nb_actors} size net is {possible_ds_nb}."
+    )
     return possible_ds_nb
 
 
 def find_real_mds(
     net_graph: nd.MultilayerNetwork,
     net_name: str,
+    min_eval_size: int,
     max_eval_size: int,
     out_dir: Path,
 ) -> None:
     """Search all possible combinations of actors up to `max_eval_size` to find dominating sets."""
     actors = net_graph.get_actors()
-    possible_ds_nb = get_possible_ds_nb(len(actors), max_eval_size)
+    possible_ds_nb = get_possible_ds_nb(len(actors), min_eval_size, max_eval_size)
     out_file = OutFile(out_dir / f"{net_name}.csv")
     p_bar = tqdm(desc="Searching possible sets", total=possible_ds_nb)
-    for n in range(max_eval_size + 1):
+    for n in range(min_eval_size, max_eval_size + 1):
         for cantidate_ds in itertools.combinations(actors, n):
             p_bar.update(1)
             with warnings.catch_warnings():
@@ -68,22 +74,24 @@ def find_real_mds(
                     out_file.save_ds(cds_ids)
 
 
-def _process_net(net: params_handler.Network, out_dir: Path) -> None:
+def _process_net(net: params_handler.Network, out_dir: Path, min_es: int, max_es: int) -> None:
     print(f"Processing {net.name} network")
     find_real_mds(
         net_graph=net.graph,
         net_name=net.name,
-        max_eval_size=net.graph.get_actors_num(),
+        min_eval_size=min_es,
+        max_eval_size=max_es,
         out_dir=out_dir,
     )
     print(f"Computations completed for: {net.name}")
 
 
 def run_experiments(config: dict[str, Any]) -> None:
-    nets = params_handler.load_networks(config["networks"])
+    networks = params_handler.load_networks(config["networks"])
     out_dir = params_handler.create_out_dir(config["logging"]["out_dir"])
+    min_es, max_es = config["es_size_range"]
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(_process_net, net, out_dir) for net in nets]
+        futures = [executor.submit(_process_net, net, out_dir, min_es, max_es) for net in networks]
         for f in concurrent.futures.as_completed(futures):
             try:
                 f.result()
