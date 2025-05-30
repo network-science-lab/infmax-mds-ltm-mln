1e9329a6a772cbb19334a1c06adb452d68ec6ca7
Michał Czuba <michal.czuba.1995@gmail.com>
2025-05-30 14:00:10 +0000

    added saving the results

--- src/real_mds.py
+++ src/real_mds.py
@@ -41,32 +41,44 @@ class OutFile:
             file.write(f"{';'.join(ds)},{len(ds)}\n")
 
 
-def main():
-    nets = load_networks(NETS)
-    print(nets)
+def get_possible_ds_nb(nb_actors: int, max_eval_size: int) -> int:
+    """Get number of solutions to evaluate."""
+    possible_ds_nb = int(np.sum([sp.binom(nb_actors, n) for n in range(max_eval_size + 1)]).item())
+    print(f"Nb of up-to-{max_eval_size} elem. sets for {nb_actors} size net is {possible_ds_nb}")
+    return possible_ds_nb
 
-    net: nd.MultilayerNetwork = nets[0].graph
-    out_file = OutFile(Path(__file__).parent.parent / f"{nets[0].name}.csv")
 
-    actors = net.get_actors()
-    max_eval_size = len(actors)
-
-    possible_ds_nb = np.sum([sp.binom(len(actors), n) for n in range(max_eval_size + 1)]).item()
-    print(
-        f"Number of up-to-{max_eval_size} element sets for "
-        f"{len(actors)}-actor size network is {possible_ds_nb}"
-    )
-
+def find_real_mds(
+    net_graph: nd.MultilayerNetwork,
+    net_name: str,
+    max_eval_size: int,
+    out_dir: Path,
+) -> None:
+    """Search all possible combinations of actors up to `max_eval_size` to find dominating sets."""
+    actors = net_graph.get_actors()
+    possible_ds_nb = get_possible_ds_nb(len(actors), max_eval_size)
+    out_file = OutFile(out_dir / f"{net_name}.csv")
     p_bar = tqdm(desc="Searching possible sets", total=possible_ds_nb)
     for n in range(max_eval_size + 1):
         for cantidate_ds in itertools.combinations(actors, n):
             p_bar.update(1)
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", category=UserWarning)
-                if is_dominating_set(set(cantidate_ds), net):
+                if is_dominating_set(set(cantidate_ds), net_graph):
                     cds_ids = {str(a.actor_id) for a in cantidate_ds}
-                    # print(f"Found {cds_ids} as a dominating set!")
                     out_file.save_ds(cds_ids)
+
+
+def main():
+    nets = load_networks(NETS)
+    for net in nets:
+        print(f"Processing {net.name} network")
+        find_real_mds(
+            net_graph=net.graph,
+            net_name=net.name,
+            max_eval_size=net.graph.get_actors_num(),
+            out_dir=Path(__file__).parent.parent,
+        )
 
 
 if __name__ == "__main__":
