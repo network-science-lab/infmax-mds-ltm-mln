800c5d72fc58f15c83805c086fb83daaeef36b9c
Michał Czuba <michal.czuba.1995@gmail.com>
2025-07-28 13:59:28 +0200

    refreshed network generator

--- src/aux/network_generator.py
+++ src/aux/network_generator.py
@@ -1,6 +1,9 @@
 """
 Generators of multilayer networks - Preferential Attachment or Erdos-Renyi.
 
+"documentation": github.com/uuinfolab/r_multinet/blob/master/man/Generation.Rd
+also, refer to this paper: `Formation of Multiple Networks` by Magnani and Rossa
+
 This is almost the same script as in network-diffusion v.0.18.1
 """
 
@@ -41,39 +44,72 @@ class MultilayerBaseGenerator(abc.ABC):
         """
         pass
 
+    def get_pr_matrices(self) -> tuple[list[float], list[float]]:
+        """
+        Create probability matrices at random.
+
+        `pr_internal` denotes probability of generation step within a layer
+        `pr_external` denotes probability of copying edge from another layer
+            according to `dependency` matrix
+        `pr_no_action` is a resulting probability of no action step and its a 
+            matrix of ones with above matrices subtracted from it.
+
+        :return: created matrices in the order: internal, external
+        """
+        pr_internal = np.random.uniform(0, 1, size=self.nb_layers).tolist()
+        pr_external = (np.ones_like(pr_internal) - pr_internal).tolist()
+        return pr_internal, pr_external
+
     def get_dependency(self) -> np.ndarray:
         """
         Create a dependency matrix for the network generator.
 
+        It generates a matrix that treats all layers equally and it's used if 
+        external step occurs during network creation.
+
         :return: matrix shaped: [nb_layers, nb_layers] with 0s in diagonal
-            and 1/nb_layers otherwise
+            and 1/(nb_layers-1) otherwise
         """
         dep = np.full(
-            fill_value=(1 / self.nb_layers),
+            fill_value=(1 / (self.nb_layers - 1)),
             shape=(self.nb_layers, self.nb_layers),
         )
         np.fill_diagonal(dep, 0)
         return dep
 
-    def __call__(self) -> PyMLNetwork:
+    def __call__(
+        self,
+        pr_internal: list[float] | None = None,
+        pr_external: list[float] | None = None,
+        dependency: list[list[float]] | None = None,
+    ) -> PyMLNetwork:
         """
         Generate the network according to given parameters.
 
         :return: generated network as a PyEvolutionModel object
         """
         models = self.get_models()
-        pr_internal = np.random.uniform(0, 1, size=self.nb_layers)
-        pr_external = (
-            np.ones_like(pr_internal) - pr_internal
-        )  # TODO: there is no noaction step now!
-        dependency = self.get_dependency()
+        if not pr_internal or not pr_internal:
+            pr_internal, pr_external = self.get_pr_matrices()
+        else:
+            assert (
+                np.array(pr_internal) + np.array(pr_external)
+                <= np.ones(len(models))
+            ).all()
+        if not dependency:
+            dependency = self.get_dependency().tolist()
+        else:
+            np.testing.assert_almost_equal(
+                np.array(dependency).sum(axis=1),
+                np.ones(len(dependency)),
+            )
         return ml.grow(
             self.nb_actors,
             self.nb_steps,
             models,
-            pr_internal.tolist(),
-            pr_external.tolist(),
-            dependency.tolist(),
+            pr_internal,
+            pr_external,
+            dependency,
         )
 
 
@@ -153,6 +189,32 @@ class MultilayerPAGenerator(MultilayerBaseGenerator):
         return [ml.evolution_pa(m0=m0, m=ms) for m0, ms in zip(m0s, ms)]
 
 
+def convert_to_nd_and_prune(net_uu: PyMLNetwork) -> MultilayerNetwork:
+    """Convert `uunet` representation to `networkx` and to `network_diffusion`."""
+    net_nx = ml.to_nx_dict(net_uu)
+    net_nd = MultilayerNetwork(layers=net_nx)
+
+    # remove selfloops from the network and actors with no connections
+    net_nd = functions.remove_selfloop_edges(net_nd)
+    actors_to_remove = [
+        ac.actor_id
+        for ac, nghb in functions.neighbourhood_size(net_nd).items()
+        if nghb == 0
+    ]
+    for l_graph in net_nd.layers.values():
+        l_graph.remove_nodes_from(actors_to_remove)
+
+    return net_nd
+
+
+def save_as_mpx(net_nd: MultilayerNetwork, path: str) -> None:
+    """Save the multilayer network as an mpx file."""
+    dummy_net = ml.empty()
+    for l_name in net_nd.layers:
+        ml.add_nx_layer(dummy_net, net_nd[l_name], l_name)
+    ml.write(dummy_net, path, "multilayer")
+
+
 def generate(
     model: Literal["PA", "ER"], nb_actors: int, nb_layers: int
 ) -> MultilayerNetwork:
@@ -170,26 +232,35 @@ def generate(
         net_uu = MultilayerPAGenerator(
             nb_layers=nb_layers,
             nb_actors=nb_actors,
-            nb_steps=nb_actors * 10,
+            nb_steps=nb_actors - nb_hubs,
             nb_hubs=nb_hubs,
         )()
     else:
-        raise ValueError("Incorret model name!")
+        raise ValueError("Incorrect model name!")
 
-    # convert uunet representation to networkx
-    net_nx = ml.to_nx_dict(net_uu)
+    return convert_to_nd_and_prune(net_uu)
 
-    # convert networkx representation to network_diffusion
-    net_nd = MultilayerNetwork(layers=net_nx)
 
-    # remove selfloops from the network and actors with no connetcions
-    net_nd = functions.remove_selfloop_edges(net_nd)
-    actors_to_remove = [
-        ac.actor_id
-        for ac, nghb in functions.neighbourhood_size(net_nd).items()
-        if nghb == 0
-    ]
-    for l_graph in net_nd.layers.values():
-        l_graph.remove_nodes_from(actors_to_remove)
+if __name__ == "__main__":
+    nb_layers=3
+    nb_actors=500
+    nb_hubs=5
+    pr_internal = [.7, .7, .7]
+    pr_external = [.2, .2, .2]
+    out_path = "./network.mpx"
 
-    return net_nd
+    net_uu = MultilayerPAGenerator(
+        nb_layers=nb_layers,
+        nb_actors=nb_actors,
+        nb_steps=nb_actors - nb_hubs,
+        nb_hubs=nb_hubs,
+    )(
+        pr_internal=pr_internal,
+        pr_external=pr_external,
+    )
+    net_nd = convert_to_nd_and_prune(net_uu)
+    save_as_mpx(net_nd, out_path)
+
+    print(net_nd)
+    net_in = MultilayerNetwork.from_mpx(out_path)
+    print(net_in)
